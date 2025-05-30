4500fedbb27e948616de26f334f428545dde3af4
aribt82 <142463431+aribt82@users.noreply.github.com>
2025-05-30 20:19:18 -0300

    update productos

--- enigma/apps/productos/urls.py
+++ enigma/apps/productos/urls.py
@@ -1,7 +1,18 @@
 # apps/usuarios/urls.py
+# from django.urls import path
+# from .views import test_view
+
+# urlpatterns = [
+    # path('test/', test_view),
+# ]
+
+
 from django.urls import path
-from .views import test_view
+from . import views
 
 urlpatterns = [
-    path('test/', test_view),
+    path('', views.listar_productos, name='listar_productos'),
+    path('nuevo/', views.crear_producto, name='crear_producto'),
+    path('editar/<int:pk>/', views.editar_producto, name='editar_producto'),
+    path('eliminar/<int:pk>/', views.eliminar_producto, name='eliminar_producto'),
 ]
